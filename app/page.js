56104c379d408a60b8b34d2748e1c81cdcbd4d68
Eth3R-One <refaythii@gmail.com>
2024-09-04 13:41:11 +0600

    add(ui): exhibitions component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
-import HeroSection from "./components/_herosection/HeroSection";
+import HeroSection from "./_components/_herosection/HeroSection";
+import ExhibitionsSection from "./_components/_exhibitions/ExhibitionsSection";
 
 export default function Home() {
-	return <HeroSection />;
+	return (
+		<>
+			<HeroSection />
+			<ExhibitionsSection />
+		</>
+	);
 }
