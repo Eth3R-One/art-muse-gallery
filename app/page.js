cb2f47b2dfd0b9fe0ea28bbe2f6155e57990cba2
Eth3R-One <refaythii@gmail.com>
2024-09-05 16:18:27 +0600

    feat: suspense and fallback loader for exhibitions and artworks section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
 import { Button } from "@/components/ui/button";
 import HeroSection from "./_components/_herosection/HeroSection";
 import ExhibitionsSection from "./_components/_exhibitions/ExhibitionsSection";
+import { Suspense } from "react";
+import SpinnerLoader from "./_components/SpinnerLoader";
+import ArtworksSection from "./_components/_artworks/ArtworksSection";
 
 export default function Home() {
 	return (
 		<>
 			<HeroSection />
-			<ExhibitionsSection />
+			<Suspense fallback={<SpinnerLoader />}>
+				<ExhibitionsSection />
+			</Suspense>
+
+			<Suspense fallback={<SpinnerLoader />}>
+				<ArtworksSection />
+			</Suspense>
 		</>
 	);
 }
