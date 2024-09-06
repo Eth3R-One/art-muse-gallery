28178619c4ef3de7345a79d7d037ba68e5867702
Eth3R-One <refaythii@gmail.com>
2024-09-07 00:49:50 +0600

    add: metadata in home page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,19 @@ import { Suspense } from "react";
 import SpinnerLoader from "./_components/SpinnerLoader";
 import ArtworksSection from "./_components/_artworks/ArtworksSection";
 
+export async function generateMetadata({ params, searchParams }, parent) {
+	return {
+		title: "Art Muse Gallery",
+		description:
+			"Located downtown by Millennium Park, this top art museum is TripAdvisor&#039;s #1 Chicago attraction—a must when visiting the city.",
+		openGraph: {
+			images: [
+				"https://artic-web.imgix.net/905abd91-5c0d-451b-9319-f7cd1505bc33/IM026911_002-web.jpg?w=1200&h=800&fit=crop",
+			],
+		},
+	};
+}
+
 export default function Home() {
 	return (
 		<>
