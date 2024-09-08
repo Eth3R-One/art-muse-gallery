443a140fb1222b4b4168c2972c3d8f850731c150
Eth3R-One <refaythii@gmail.com>
2024-09-08 12:52:13 +0600

    add: added exhibition details page with id

--- app/_components/_exhibitions/ExhibitionsSection.jsx
+++ app/_components/_exhibitions/ExhibitionsSection.jsx
@@ -33,34 +33,36 @@ const ExhibitionsSection = async () => {
 								className="px-2 md:px-4 md:basis-1/2 lg:basis-1/6 cursor-pointer flex flex-col justify-center "
 								title={exhibition?.title}
 							>
-								<div className="rounded-lg flex flex-col justify-between hover:scale-110">
-									<Card className="relative rounded-lg">
-										<CardContent className="relative aspect-square overflow-hidden">
-											<Image
-												src={exhibition?.image_url ?? NoImage}
-												alt={exhibition?.title}
-												layout="fill"
-												objectFit="cover"
-												className="rounded-lg"
-												sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-											/>
-											<div className="absolute bottom-2 right-2 bg-black bg-opacity-60 text-white text-xs px-2 py-1 rounded">
-												<p>{exhibition?.status}</p>
-											</div>
-										</CardContent>
-										<div className="border bg-[#EFEDE0] flex flex-col justify-between">
-											<p className="text-xl font-mono px-5">
-												{exhibition?.title}
-											</p>
+								<Link href={`/exhibitions/${exhibition?.id}`}>
+									<div className="rounded-lg flex flex-col justify-between hover:scale-110">
+										<Card className="relative rounded-lg">
+											<CardContent className="relative aspect-square overflow-hidden">
+												<Image
+													src={exhibition?.image_url ?? NoImage}
+													alt={exhibition?.title}
+													layout="fill"
+													objectFit="cover"
+													className="rounded-lg"
+													sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+												/>
+												<div className="absolute bottom-2 right-2 bg-black bg-opacity-60 text-white text-xs px-2 py-1 rounded">
+													<p>{exhibition?.status}</p>
+												</div>
+											</CardContent>
+											<div className="border bg-[#EFEDE0] flex flex-col justify-between">
+												<p className="text-xl font-mono px-5">
+													{exhibition?.title}
+												</p>
 
-											<p className="text-sm font-sans px-5">
-												({formatDate(exhibition?.aic_start_at)}{" "}
-												{"-"}
-												{formatDate(exhibition?.aic_end_at)})
-											</p>
-										</div>
-									</Card>
-								</div>
+												<p className="text-sm font-sans px-5">
+													({formatDate(exhibition?.aic_start_at)}{" "}
+													{"-"}
+													{formatDate(exhibition?.aic_end_at)})
+												</p>
+											</div>
+										</Card>
+									</div>
+								</Link>
 							</CarouselItem>
 						))}
 					</CarouselContent>
--- app/exhibitions/_components/ExhibitionCard.jsx
+++ app/exhibitions/_components/ExhibitionCard.jsx
@@ -8,6 +8,7 @@ import {
 
 import NoImage from "../../../public/images/no_photo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const ExhibitionCard = ({ exhibition }) => {
 	const short_description = exhibition?.short_description
@@ -19,26 +20,28 @@ const ExhibitionCard = ({ exhibition }) => {
 			className="w-full max-w-xs hover:scale-110 hover:transition-transform cursor-pointer rounded-xl"
 			title={exhibition?.title}
 		>
-			<Image
-				src={exhibition?.image_url ?? NoImage}
-				alt={exhibition?.title}
-				width="400"
-				height="250"
-				className="aspect-[1.6] rounded-xl"
-			/>
-
-			<div className="flex flex-col justify-start px-2 py-1">
-				<p className="sm:md:text-2xl items-center font-mono text-lg py-2 text-center text-wrap">
-					{exhibition?.title?.slice(0, 40)}{" "}
-					{exhibition?.title?.length > 40 && " ...see more"}
-				</p>
-			</div>
-			<span className="px-3 text-wrap">
-				<p
-					className="px-5 text-center"
-					dangerouslySetInnerHTML={{ __html: short_description }}
+			<Link href={`/exhibitions/${exhibition?.id}`}>
+				<Image
+					src={exhibition?.image_url ?? NoImage}
+					alt={exhibition?.title}
+					width="400"
+					height="250"
+					className="aspect-[1.6] rounded-xl"
 				/>
-			</span>
+
+				<div className="flex flex-col justify-start px-2 py-1">
+					<p className="sm:md:text-2xl items-center font-mono text-lg py-2 text-center text-wrap">
+						{exhibition?.title?.slice(0, 40)}{" "}
+						{exhibition?.title?.length > 40 && " ...see more"}
+					</p>
+				</div>
+				<span className="px-3 text-wrap">
+					<p
+						className="px-5 text-center"
+						dangerouslySetInnerHTML={{ __html: short_description }}
+					/>
+				</span>
+			</Link>
 		</Card>
 	);
 };
