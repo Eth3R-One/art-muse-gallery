a93871c0641e5544dc140532e604b3f269a10669
Eth3R-One <refaythii@gmail.com>
2024-09-08 12:43:46 +0600

    removed: comments

--- app/exhibitions/_components/ExhibitionCard.jsx
+++ app/exhibitions/_components/ExhibitionCard.jsx
@@ -28,31 +28,17 @@ const ExhibitionCard = ({ exhibition }) => {
 			/>
 
 			<div className="flex flex-col justify-start px-2 py-1">
-				{/* <CardHeader className="p-4"> */}
 				<p className="sm:md:text-2xl items-center font-mono text-lg py-2 text-center text-wrap">
 					{exhibition?.title?.slice(0, 40)}{" "}
 					{exhibition?.title?.length > 40 && " ...see more"}
 				</p>
-
-				{/* </CardHeader> */}
-				{/* <p className="font-normal bg-rose-200 text-rose-600 rounded-md px-2">
-					{exhibition?.status}
-				</p> */}
 			</div>
 			<span className="px-3 text-wrap">
-				{/* <p className="px-5 text-center">
-					{exhibition?.short_description
-						? exhibition?.short_description?.slice(0, 100)
-						: "** No description found **"}
-				</p> */}
 				<p
 					className="px-5 text-center"
 					dangerouslySetInnerHTML={{ __html: short_description }}
 				/>
 			</span>
-			{/* <CardContent className="p-4">
-				<p className="text-2xl font-semibold">$199.00</p>
-			</CardContent> */}
 		</Card>
 	);
 };
