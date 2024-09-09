7ccbb87725a8900cab2010333ae96b999147af1e
Eth3R-One <refaythii@gmail.com>
2024-09-09 13:11:01 +0600

    add: action to get exhibition details by id

--- app/actions/exhibitionsActions/index.js
+++ app/actions/exhibitionsActions/index.js
@@ -11,3 +11,48 @@ export const getExhibitions = async ({ LIMIT = 0, PAGE = 1 } = {}) => {
 		// throw err;
 	}
 };
+
+export const getExhibitionById = async (exhibitionId) => {
+	try {
+		const response = await fetch(
+			`${process.env.API_BASE_URL}/exhibitions/${exhibitionId}`
+		);
+
+		const exhibition = await response.json();
+		return exhibition?.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const getExhibitionImagesById = async (exhibition) => {
+	try {
+		// const response = await fetch(
+		// 	`${process.env.API_BASE_URL}/exhibitions/${exhibitionId}?fields=id,title,image_id,alt_image_ids`
+		// );
+
+		// const exhibition = await response.json();
+		if (exhibition?.image_id) {
+			return [exhibition?.image_id, ...exhibition?.alt_image_ids];
+		} else {
+			return [...exhibition?.alt_image_ids];
+		}
+	} catch (err) {
+		console.log(err);
+	}
+};
+// export const getExhibitionImagesById = async (exhibitionId) => {
+// 	try {
+// 		const response = await fetch(
+// 			`${process.env.API_BASE_URL}/exhibitions/${exhibitionId}?fields=id,title,image_id,alt_image_ids`
+// 		);
+
+// 		const exhibition = await response.json();
+// 		const exhibitionImages = exhibition?.data?.image_id
+// 			? [exhibition?.data?.image_id]
+// 			: [];
+// 		return [exhibitionImages, ...exhibition?.data?.alt_image_ids];
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// };
