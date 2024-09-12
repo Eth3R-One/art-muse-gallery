c9f09bc4bc4c1c02187bdfd2678f3332daea8d46
Eth3R-One <refaythii@gmail.com>
2024-09-12 14:57:58 +0600

    add: artwork actions to get artworks and a single artwork by id

--- app/actions/artworksActions/index.js
+++ app/actions/artworksActions/index.js
@@ -1,10 +1,25 @@
-export const getArtworks = async () => {
+export const getArtworks = async ({ PAGE = 1, LIMIT = 0 } = {}) => {
 	try {
-		const data = await fetch(`${process.env.API_BASE_URL}/artworks/`);
+		const data = await fetch(
+			`${process.env.API_BASE_URL}/artworks/?limit=${LIMIT}&page=${PAGE}`
+		);
 		const artworks = await data.json();
 
 		return artworks?.data;
 	} catch (err) {
 		console.error("there's something went wrong getting artworks! ", err);
+	}
+};
+
+export const getArtworkById = async (artworkId) => {
+	try {
+		const response = await fetch(
+			`${process.env.API_BASE_URL}/artworks/${artworkId}`
+		);
+
+		const artwork = await response.json();
+		return artwork?.data;
+	} catch (err) {
+		console.log(err);
 	}
 };
