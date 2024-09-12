d999154a9490fd6acab4fbd7c295cfa8a83f5c3c
Eth3R-One <refaythii@gmail.com>
2024-09-12 23:03:58 +0600

    add: metadata to artworks page

--- app/artworks/page.js
+++ app/artworks/page.js
@@ -13,6 +13,17 @@ import { getArtworks } from "@/app/actions/artworksActions";
 import Link from "next/link";
 import ArtworksList from "./_components/ArtworksList";
 
+export async function generateMetadata({ params, searchParams }, parent) {
+	return {
+		title: "Artworks | Art Muse Gallery",
+		openGraph: {
+			images: [
+				"https://artic-web.imgix.net/905abd91-5c0d-451b-9319-f7cd1505bc33/IM026911_002-web.jpg?w=1200&h=800&fit=crop",
+			],
+		},
+	};
+}
+
 const ArtworksPage = async () => {
 	const artworks = await getArtworks();
 	return (
