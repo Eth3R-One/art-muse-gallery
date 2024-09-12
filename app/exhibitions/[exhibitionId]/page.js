9838e167e2da47c035a76904db9879d69660e7f6
Eth3R-One <refaythii@gmail.com>
2024-09-13 00:08:15 +0600

    add: dynamic metadata to exhibition page

--- app/exhibitions/[exhibitionId]/page.js
+++ app/exhibitions/[exhibitionId]/page.js
@@ -3,6 +3,26 @@ import {
 	getExhibitionImages,
 } from "@/app/actions/exhibitionsActions";
 import ExhibitionDetails from "./_components/ExhibitionDetails";
+import { joinImageUrl } from "@/utils/image-url";
+
+export async function generateMetadata({ params: { exhibitionId } }) {
+	const exhibition = await getExhibitionById(exhibitionId);
+	return {
+		title: `${
+			exhibition?.title ? exhibition?.title : "NO TITLE"
+		} | Art Muse Gallery`,
+		description:
+			exhibition?.description ??
+			exhibition?.short_description ??
+			exhibition?.title,
+		openGraph: {
+			images: [
+				exhibition?.image_url ??
+					"https://artic-web.imgix.net/905abd91-5c0d-451b-9319-f7cd1505bc33/IM026911_002-web.jpg?w=1200&h=800&fit=crop",
+			],
+		},
+	};
+}
 
 const ExhibitionPage = async ({ params: { exhibitionId } }) => {
 	const exhibition = await getExhibitionById(exhibitionId);
